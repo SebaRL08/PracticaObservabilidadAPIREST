f34ab426d8c9f8fc4fd1f7e98091ff3785c3d450
rocha231 <santiagorocha.g@academia.umb.edu.co>
2026-09-12 22:46:02 -0500

    documentacion de API.py

--- API.py
+++ API.py
@@ -1,5 +1,15 @@
+"""
+Módulo para iniciar el servidor de la aplicación.
+
+Este archivo permite ejecutar la API utilizando Uvicorn como servidor.
+Está configurado para trabajar de forma local y reiniciar automáticamente
+el servidor cuando se realicen cambios en el código durante el desarrollo.
+"""
+
 import uvicorn
 
 if __name__ == "__main__":
     uvicorn.run("app.mainApp:app", host="127.0.0.1", port=8000, reload=True)
+
+
 
